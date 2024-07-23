61a15a3beac1623108771eda95d33629eb936507
zelalem3 <zgetnet24@example.com>
2024-07-23 10:24:17 -0700

    add files

---
+++ 0x05-Node_JS_basic/3-main_1.js
@@ -0,0 +1,12 @@
+
+const countStudents = require('./3-read_file_async');
+
+countStudents("database.csv")
+    .then(() => {
+        console.log("Done!");
+    })
+        .catch((error) => {
+        console.log(error);
+    });
+console.log("After!");
+
