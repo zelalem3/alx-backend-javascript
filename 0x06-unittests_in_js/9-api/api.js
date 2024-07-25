89cf2dd415343ab4b7bbba5427f44ef7e0dcbec1
zelalem3 <zgetnet24@example.com>
2024-07-25 11:05:06 -0700

    add files

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,16 +1,20 @@
-
 const express = require('express');
+
 const app = express();
-const port = 7865;
+const PORT = 7865;
 
-app.get('/', (req, res) => {
-  res.end('Welcome to the payment system');
+app.get('/', (_, res) => {
+  res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id([0-9]+)', (req, res) => {
-  res.end(`Payment methods for cart ${req.params.id}`);
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const id = req.params.id;
+
+  res.send(`Payment methods for cart ${id}`);
 });
 
-app.listen(port, () => {
-  console.log('API available on localhost port 7865');
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
 });
+
+module.exports = app;
