e5f248d995e02b179dcb1fd4087691f74526bee3
zelalem3 <zgetnet24@example.com>
2024-07-25 11:16:56 -0700

    edit files

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,1 +1,11 @@
-/* eslint-disable */
+/**
+ * Miscellaneous route.
+ */
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
+}
+
+export default AppController;
+module.exports = AppController;
